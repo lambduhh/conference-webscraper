de41608a7ee5caf7283647b12d4228e1193f7f60
L. Jordan Miller <lorilynjmiller@gmail.com>
2024-08-14 01:03:35 -0400

    wip generates csv w only columns

--- tech_events_scraper.clj
+++ tech_events_scraper.clj
@@ -9,27 +9,30 @@
     (html/html-resource (java.io.StringReader. (:body response)))))
 
 (defn extract-events [page]
-  (let [event-nodes (html/select page [:div.event-card])]
+  (let [event-nodes (html/select page [:div.EventCard])]
     (for [event event-nodes]
-      (let [name (first (html/select event [:h3]))
-            location (first (html/select event [:span.location]))
-            link (first (html/select event [:a]))
-            date (first (html/select event [:span.date]))]
+      (let [name (first (html/select event [:h3.EventCard-title]))
+            location (first (html/select event [:div.EventCard-location]))
+            link-node (first (html/select event [:a]))
+            date (first (html/select event [:time]))
+            link (if link-node (:href (:attrs link-node)))]
         {:name (html/text name)
          :location (if location (html/text location) "N/A")
-         :link (if link (html/attr= link :href) "N/A")
+         :link (if link (str "https://dev.events" link) "N/A")
          :date (if date (html/text date) "N/A")}))))
-
 
 (defn write-csv [filename data]
   (with-open [writer (io/writer filename)]
-    (csv/write-csv writer (map #(vector (:name %) (:location %) (:link %) (:date %)) data))))
+    (csv/write-csv writer
+                   ;; Ensure header and each row are fully realized vectors
+                   (cons ["Name" "Location" "Link to website" "Estimated date"]
+                         (mapv #(vector (:name %) (:location %) (:link %) (:date %)) data)))))
 
 (defn -main []
-  (let [url "https://www.bizzabo.com/blog/technology-events"
+  (let [url "https://dev.events/"
         page (fetch-page url)
         events (extract-events page)]
-    (write-csv "technology_events.csv" events)
+    (write-csv "dev_events.csv" events)
     (println "CSV file created successfully.")))
 
 ;; To run the script
